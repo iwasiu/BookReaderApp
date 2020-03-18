44a27ba278803744b6005b1db016672fd532c003
iwasiu <iwasiu460@gmail.com>
2020-03-18 15:46:56 +0100

    Constructor

--- Book.java
+++ Book.java
@@ -1,6 +1,13 @@
 package BookReaderApp;
 
 public class Book {
+    String title ;
+    enum publisher = {MACMILLAN, PEARSON, PRINCETON,MAGOOSH,BARRON} ;
+    enum subtitle = {ENGLISH, FRENCH, LATIN, SWAHILI, HAUSA, ARABIC} ;
+    String abstract = null ;
+    enum scope = {ADVENTURE, SCIENTIFIC, POLITICAL} ;
+    String edition ;
+    
     int isbn ;
     String author ;
 
