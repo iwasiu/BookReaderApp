63dc6e432afa108fab0ebc560b777b0409ef3b9f
iwasiu <iwasiu460@gmail.com>
2020-03-18 15:58:21 +0100

    constructor

--- Book.java
+++ Book.java
@@ -4,14 +4,24 @@ public class Book {
     String title ;
     enum publisher = {MACMILLAN, PEARSON, PRINCETON,MAGOOSH,BARRON} ;
     enum subtitle = {ENGLISH, FRENCH, LATIN, SWAHILI, HAUSA, ARABIC} ;
-    String abstract = null ;
+    String abstractstmt ;
     enum scope = {ADVENTURE, SCIENTIFIC, POLITICAL} ;
     String edition ;
-    
+    int numberOfPages ;
+    enum FormatText = {TIMES_NEW_ROMAN, COMIC_SANS, ARIAL} ;
+    String copyrightHolder ;
+    Date dateCreated ; 
     int isbn ;
     String author ;
-
-
+        public Book(String title, String abstractstmt, String edition, int numberOfPages, String copyrightHolder, int isbn, String author){
+             this.title = title; 
+             this.abstractstmt = abstractstmt; 
+             this.edition = edition;
+             this.numberOfPages = numberOfPages;
+            this.copyrightHolder = copyrightHolder; 
+            this.isbn = isbn; 
+            this.author = author;
+        }
     public addBookLibrary(){
 
         
