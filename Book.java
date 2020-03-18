52c3d0d6042b27278208b623eb2911045f045d52
iwasiu <iwasiu460@gmail.com>
2020-03-18 17:53:43 +0100

    constructor overloading for book class

--- Book.java
+++ Book.java
@@ -3,10 +3,10 @@ package BookReaderApp;
 public class Book {
     public String title;
     public enum publisher {MACMILLAN, PEARSON, PRINCETON, MAGOOSH, BARRON};
-    public String description;
+    public String description = "Description not given";
     public int numberOfPages;
     public int isbn;
-    public String author;
+    public String author = "Anonymous";
 
     public Book(String author, String title, String description, int numberOfPages, int isbn) {
         this.title = title;
@@ -14,5 +14,20 @@ public class Book {
         this.author = author;
         this.numberOfPages = numberOfPages;
         this.isbn = isbn;
+    }
+    
+    public Book(String author, String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+    }
+
+    public Book(String author, String title) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(String title) {
+        this.title = title;
     }
 }
